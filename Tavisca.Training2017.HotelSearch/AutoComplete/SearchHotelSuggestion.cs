48b295039be887651255e39f59d8c68db34e8df2
Prabhash Mohanty <pmohanty@tavisca.com>
2017-10-06 18:58:03 +0530

    Made some changes to the searchhotel

--- Tavisca.Training2017.HotelSearch/AutoComplete/SearchHotelSuggestion.cs
+++ Tavisca.Training2017.HotelSearch/AutoComplete/SearchHotelSuggestion.cs
@@ -12,13 +12,13 @@ namespace AutoComplete
     {
         public async Task<string[]> GetSearchQueryData(string searchTerm)
         {
-            searchTerm=searchTerm.ToUpper();
-            WebRequest req = WebRequest.Create(@"http://portal.dev-rovia.com/Services/api/Content/GetAutoCompleteDataGroups?type=hotel%7Cpoi&query="+searchTerm);
+            searchTerm = searchTerm.ToUpper();
+            WebRequest req = WebRequest.Create(@"http://portal.dev-rovia.com/Services/api/Content/GetAutoCompleteDataGroups?type=hotel%7Cpoi&query=" + searchTerm);
 
             req.Method = "GET";
             string s = "";
             WebResponse resp = await req.GetResponseAsync();
-            if (resp.ContentLength !=0)
+            if (resp.ContentLength != 0)
             {
                 using (Stream respStream = resp.GetResponseStream())
                 {
@@ -27,16 +27,16 @@ namespace AutoComplete
                 }
             }
             string reg = "\"ItemList\":\\[";
-            string[] str = Regex.Split(s,reg);
-            string[] splitString = Regex.Split(str[1],"},",RegexOptions.IgnoreCase);
+            string[] str = Regex.Split(s, reg);
+            string[] splitString = Regex.Split(str[1], "},", RegexOptions.IgnoreCase);
             string[] hotelList = ParseHotelList(splitString);
             return hotelList;
         }
 
         private string[] ParseHotelList(string[] splitString)
         {
-            string[] hotelList= new string[splitString.Length];
-            for(int i=0;i<splitString.Length;i++)
+            string[] hotelList = new string[splitString.Length];
+            for (int i = 0; i < splitString.Length; i++)
             {
                 string str = splitString[i];
                 string []s = Regex.Split(str, ",\"SubItemList\":");
