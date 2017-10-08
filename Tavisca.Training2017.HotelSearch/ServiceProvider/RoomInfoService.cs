8dfeef4807492bf05493112607087598dbc293c9
Suraj Narayan <snarayan@tavisca.com>
2017-10-08 17:36:05 +0530

    Implementation of the Room-Request Search &Parser

--- Tavisca.Training2017.HotelSearch/ServiceProvider/RoomInfoService.cs
+++ Tavisca.Training2017.HotelSearch/ServiceProvider/RoomInfoService.cs
@@ -1,18 +1,28 @@
-﻿using System;
+﻿using HotelEngienSearch;
+using HotelSearchEngine;
+using HotelSearchEngine.Model;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ServiceProvider
 {
-    public class RoomInfoService 
+    public class RoomInfoService :IHotelService
     {
 
-        //List<RoomSearch> roomIterneries;
-        //public Task<string> GetData(string searchTerm)
-        //{
-           
-
-        //}
-    }
+        HotelRoomAvailResponse roomItinaries;
+        public RoomInfoService()
+        {
+            roomItinaries = new HotelRoomAvailResponse();
+        }
+        public async Task<string> GetData(string searchTerm)
+        {
+            RoomSearch search = new RoomSearch();
+            var request = JsonConvert.DeserializeObject<HotelListingResponse>(searchTerm);
+            roomItinaries=await search.GetRoomDetails(request);
+            return JsonConvert.SerializeObject(roomItinaries);
+        }
+        }
 }
