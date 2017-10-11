d181284a9eb9f98aa1852edc88bca494b5c75369
Prabhash Mohanty <pmohanty@tavisca.com>
2017-10-11 09:19:11 +0530

    Refactored and simplified the code

--- Tavisca.Training2017.HotelSearch/AutoComplete/SearchHotelSuggestion.cs
+++ Tavisca.Training2017.HotelSearch/AutoComplete/SearchHotelSuggestion.cs
@@ -12,7 +12,6 @@ namespace AutoComplete
     {
         public async Task<string> GetSearchQueryData(string searchTerm)
         {
-            //searchTerm = searchTerm.ToUpper();
             WebRequest req = WebRequest.Create(@"http://portal.dev-rovia.com/Services/api/Content/GetAutoCompleteDataGroups?type=city|airport|poi&query=" + searchTerm);
 
             req.Method = "GET";
@@ -29,38 +28,5 @@ namespace AutoComplete
            
             return hotelList;
         }
-
-        //private List<string> ParseHotelList(string[] splitString1,string[] splitString2,string[] splitString3)
-        //{
-        //    List<string> hotelList = new List<string>();
-        //    for (int i = 0; i < splitString1.Length; i++)
-        //    {
-        //        string str = splitString1[i];
-        //        string []s = Regex.Split(str, ",\"SubItemList\":");
-        //       for(int j=0;j<s.Length;j++)
-        //        {
-        //            hotelList.Add(s[j]);
-        //        }
-        //    }
-        //    for (int i = 0; i < splitString2.Length; i++)
-        //    {
-        //        string str = splitString1[i];
-        //        string[] s = Regex.Split(str, ",\"SubItemList\":");
-        //        for (int j = 0; j < s.Length; j++)
-        //        {
-        //            hotelList.Add(s[j]);
-        //        }
-        //    }
-        //    for (int i = 0; i < splitString3.Length; i++)
-        //    {
-        //        string str = splitString1[i];
-        //        string[] s = Regex.Split(str, ",\"SubItemList\":");
-        //        for (int j = 0; j < s.Length; j++)
-        //        {
-        //            hotelList.Add(s[j]);
-        //        }
-        //    }
-        //    return hotelList;
-        //}
     }
 }
--- Tavisca.Training2017.HotelSearch/HotelSearchEngine/RoomRequestParser.cs
+++ Tavisca.Training2017.HotelSearch/HotelSearchEngine/RoomRequestParser.cs
@@ -22,20 +22,9 @@ namespace HotelSearchEngine
         }
         public HotelRoomAvailRQ Parser(RoomListingRequest request)
         {
-            //var stream = File.OpenText(@"D:\Hotel_Search_Project\Tavisca.Training2017.HotelSearch\HotelSearchEngine\SessionLog\HotelSearchCriterion_Json.txt");
-            //string data = stream.ReadToEnd();
-            //var sessionDataList = JsonConvert.DeserializeObject<SessionData>(data);
             roomRequest.ResultRequested = ResponseType.Complete;
             roomRequest.SessionId = request.SessionId;
             roomRequest.Itinerary = request.Itinerary;
-            //foreach (var sessionData in sessionDataList)
-            //{
-            //    if (sessionData.SessionId.Equals(request.SessionId))
-            //    {
-            //        roomRequest.HotelSearchCriterion = sessionData.HotelSearchCriterionData;
-            //        break;
-            //    }
-            //}
             roomRequest.HotelSearchCriterion = request.HotelCriterion;
             return roomRequest;
         }
