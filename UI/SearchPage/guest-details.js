7216216b051746ec94a9aa9d116abec79334ee0f
Prabhash Mohanty <pmohanty@tavisca.com>
2017-10-13 15:43:26 +0530

    Validations of some fields of the guest-details

--- UI/SearchPage/guest-details.js
+++ UI/SearchPage/guest-details.js
@@ -7,13 +7,13 @@ $(document).ready(function(){
 	var outDate=updatedData.data.HotelItinerary.StayPeriod.End.toString().split('T');
 	var currencyType=updatedData.data.HotelItinerary.Rooms[0].DisplayRoomRate.TotalFare.Currency;
 	var htmlData={
-		hotelName:updatedData.data.HotelItinerary.HotelProperty.Name.toString(),
-	 	roomType:updatedData.data.HotelItinerary.Rooms[0].RoomName.toString(),
-	 	numOfRooms:updatedData.rooms.toString(),
-	 	checkInDate:inDate[0].toString(),
-	 	checkOutDate:outDate[0].toString(),
-	 	duration:updatedData.data.HotelItinerary.StayPeriod.Duration.toString(),
-	 	amount:(currencyType+" "+updatedData.data.HotelItinerary.Rooms[0].DisplayRoomRate.TotalFare.Amount).toString()
+		hotelName:" "+updatedData.data.HotelItinerary.HotelProperty.Name.toString(),
+	 	roomType:" "+updatedData.data.HotelItinerary.Rooms[0].RoomName.toString(),
+	 	numOfRooms:" "+updatedData.rooms.toString(),
+	 	checkInDate:" "+inDate[0].toString(),
+	 	checkOutDate:" "+outDate[0].toString(),
+	 	duration:" "+updatedData.data.HotelItinerary.StayPeriod.Duration.toString(),
+	 	amount:" "+(currencyType+" "+updatedData.data.HotelItinerary.Rooms[0].DisplayRoomRate.TotalFare.Amount).toString()
 }
 
 var template = $('#itinerary-details');
@@ -25,9 +25,24 @@ var template = $('#itinerary-details');
   $('#booking-details').html(html);
   $("#booking").click(function(){
   			var cardNumber=$("#cardNum").val();
-  			var cvv=$("#cvv").value;
+  			var cvv=$("#cvv").val();
   			var mobileNum=$("#mobile").val();
   			var cardDigits=cardNumber.split('');
   			var mobileDigits=mobileNum.split('');
+  			if(mobileNum.length>10 || mobileNum.length<10)
+  			{
+  				alert("Please enter a valid mobile number");
+  				return;
+  			}
+  			if(cardDigits.length>16 || cardDigits.length<16)
+  			{
+  				alert("Please enter a valid credit card number");
+  				return;
+  			}
+  			if(cvv.length>3 || cardDigits.length<3)
+  			{
+  				alert("Please enter a valid cvv number");
+  				return;
+  			}
   });
 });
