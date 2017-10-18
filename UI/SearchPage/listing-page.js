eced2f30f0296a902bbb4cb3c0d64bbd0052f0aa
Prabhash Mohanty <pmohanty@tavisca.com>
2017-10-18 08:51:36 +0530

    Modified listing page css

--- UI/SearchPage/listing-page.js
+++ UI/SearchPage/listing-page.js
@@ -117,7 +117,7 @@ Handlebars.registerHelper('times', function (n, block) {
 			name:result[i].itinerary.HotelProperty.Name,
 			city:result[i].itinerary.HotelProperty.Address.City.Name,
 			rating:result[i].itinerary.HotelProperty.HotelRating.Rating,
-			price:result[i].itinerary.Fare.TotalFare.Currency+" "+result[i].itinerary.Fare.TotalFare.Amount,
+			price:result[i].itinerary.Fare.TotalFare.BaseEquivCurrency+" "+result[i].itinerary.Fare.TotalFare.UsdEquivAmount,
 			});
 		}
 		}
