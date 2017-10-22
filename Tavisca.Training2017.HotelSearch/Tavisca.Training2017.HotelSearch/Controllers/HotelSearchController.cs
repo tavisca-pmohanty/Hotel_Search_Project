750d5faa75382cc914a95e8228ada8412f350ee7
Suraj Narayan <snarayan@tavisca.com>
2017-10-22 19:26:17 +0530

    Added  ExceptionHandling of AutoController

--- Tavisca.Training2017.HotelSearch/Tavisca.Training2017.HotelSearch/Controllers/HotelSearchController.cs
+++ Tavisca.Training2017.HotelSearch/Tavisca.Training2017.HotelSearch/Controllers/HotelSearchController.cs
@@ -22,10 +22,17 @@ namespace Tavisca.Training2017.HotelSearch.Controllers
             }
             else
             {
-                ServiceRepository repository = new ServiceRepository();
-                var service = repository.GetService("HotelListing");
-                string hotelListing = await service.GetRequestedDataAsync(requestData);
-                await HttpContext.Response.WriteAsync(hotelListing);
+                try
+                {
+                    ServiceRepository repository = new ServiceRepository();
+                    var service = repository.GetService("HotelListing");
+                    string hotelListing = await service.GetRequestedDataAsync(requestData);
+                    await HttpContext.Response.WriteAsync(hotelListing);
+                }
+                catch(Exception ex)
+                {
+                    Logger.Log.LogError(ex);
+                }
             }
         }
 
@@ -40,10 +47,17 @@ namespace Tavisca.Training2017.HotelSearch.Controllers
             }
             else
             {
-                ServiceRepository repository = new ServiceRepository();
-                var service = repository.GetService("HotelRooms");
-                string hotelListing = await service.GetRequestedDataAsync(requestData);
-                await HttpContext.Response.WriteAsync(hotelListing);
+                try
+                {
+                    ServiceRepository repository = new ServiceRepository();
+                    var service = repository.GetService("HotelRooms");
+                    string hotelListing = await service.GetRequestedDataAsync(requestData);
+                    await HttpContext.Response.WriteAsync(hotelListing);
+                }
+                catch(Exception ex)
+                {
+                    Logger.Log.LogError(ex);
+                }
             }
         }
 
@@ -57,47 +71,64 @@ namespace Tavisca.Training2017.HotelSearch.Controllers
             }
             else
             {
-                ServiceRepository repository = new ServiceRepository();
-                var service = repository.GetService("RoomPricing");
-                string roomPricingData = await service.GetRequestedDataAsync(requestData);
-                await HttpContext.Response.WriteAsync(roomPricingData);
+                try
+                {
+                    ServiceRepository repository = new ServiceRepository();
+                    var service = repository.GetService("RoomPricing");
+                    string roomPricingData = await service.GetRequestedDataAsync(requestData);
+                    await HttpContext.Response.WriteAsync(roomPricingData);
+                }
+                catch(Exception ex)
+                {
+                    Logger.Log.LogError(ex);
+                }
             }
         }
         [Route("GetBookingDetails")]
         [HttpPost]
         public async Task GetBookingDetail([FromBody] string requestData)
         {
+            try
+            {
 
-            ServiceRepository repository = new ServiceRepository();
-            var service = repository.GetService("CompleteBooking");
-            string bookingData = await service.GetRequestedDataAsync(requestData);
-            await HttpContext.Response.WriteAsync(bookingData);
+                ServiceRepository repository = new ServiceRepository();
+                var service = repository.GetService("CompleteBooking");
+                string bookingData = await service.GetRequestedDataAsync(requestData);
+                await HttpContext.Response.WriteAsync(bookingData);
+            }
+            catch(Exception ex)
+            {
+                Logger.Log.LogError(ex);
+            }
         }
 
         [Route("index/")]
         public class CompleteBooKingParser : Controller
         {
             [HttpGet("book")]
+
             public async Task<CompleteBookingRS> BookRequest()
             {
-                CompleteBookingRQ CompleteBookingRequestParser = new CompleteBookingRQ()
+                try
                 {
-                    ResultRequested = ResponseType.Unknown,
-                    SessionId = Guid.NewGuid().ToString(),
-                    ExternalPayment = new CreditCardPayment()
+                    CompleteBookingRQ CompleteBookingRequestParser = new CompleteBookingRQ()
                     {
-                        Amount = new Money()
+                        ResultRequested = ResponseType.Unknown,
+                        SessionId = Guid.NewGuid().ToString(),
+                        ExternalPayment = new CreditCardPayment()
                         {
-                            BaseEquivAmount = 0M,
-                            BaseEquivCurrency = "USD",
-                            Currency = "USD",
-                            DisplayAmount = 41.20M,
-                            Amount = 41.20M,
-                            DisplayCurrency = "USD",
-                            UsdEquivAmount = 0M
-                        },
-                        Attributes = new StateBag[]
+                            Amount = new Money()
                             {
+                                BaseEquivAmount = 0M,
+                                BaseEquivCurrency = "USD",
+                                Currency = "USD",
+                                DisplayAmount = 41.20M,
+                                Amount = 41.20M,
+                                DisplayCurrency = "USD",
+                                UsdEquivAmount = 0M
+                            },
+                            Attributes = new StateBag[]
+                                {
                        new StateBag()
                        {
                            Name ="PointOfSale",
@@ -164,46 +195,58 @@ namespace Tavisca.Training2017.HotelSearch.Controllers
                            Name ="_AttributeRule_Rovia_Password",
                            Value = "true"
                        },
-                            },
-                        BillingAddress = new Address()
-                        {
-                            CodeContext = LocationCodeContext.Address,
-                            GmtOffsetMinutes = 0,
-                            Id = 0,
-                            AddressLine1 = "5360 Legacy Drive Suite 300",
-                            City = new City
+                                },
+                            BillingAddress = new Address()
                             {
-                                CodeContext = LocationCodeContext.City,
+                                CodeContext = LocationCodeContext.Address,
                                 GmtOffsetMinutes = 0,
                                 Id = 0,
-                                Name = "Plano",
-                                Country = "US",
-                                State = "TX"
+                                AddressLine1 = "5360 Legacy Drive Suite 300",
+                                City = new City
+                                {
+                                    CodeContext = LocationCodeContext.City,
+                                    GmtOffsetMinutes = 0,
+                                    Id = 0,
+                                    Name = "Plano",
+                                    Country = "US",
+                                    State = "TX"
+                                },
+                                PhoneNumber = "1214 - 231 - 5445",
+                                ZipCode = "75024"
                             },
-                            PhoneNumber = "1214 - 231 - 5445",
-                            ZipCode = "75024"
+                            Id = Guid.Parse("00000000-0000-0000-0000-000000000000"),
+                            PaymentType = PaymentType.Credit,
+                            Rph = 0,
+                            CardMake = new CreditCardMake()
+                            {
+                                Code = "VI",
+                                Name = "Visa"
+                            },
+                            CardType = CreditCardType.Personal,
+                            ExpiryMonthYear = new DateTime(2019, 01, 01),
+                            IsThreeDAuthorizeRequired = false,
+                            NameOnCard = "Saurabh Cache",
+                            Number = "0000000000001111",
+                            SecurityCode = "123"
                         },
-                        Id = Guid.Parse("00000000-0000-0000-0000-000000000000"),
-                        PaymentType = PaymentType.Credit,
-                        Rph = 0,
-                        CardMake = new CreditCardMake()
-                        {
-                            Code = "VI",
-                            Name = "Visa"
-                        },
-                        CardType = CreditCardType.Personal,
-                        ExpiryMonthYear = new DateTime(2019, 01, 01),
-                        IsThreeDAuthorizeRequired = false,
-                        NameOnCard = "Saurabh Cache",
-                        Number = "0000000000001111",
-                        SecurityCode = "123"
-                    },
-                    TripFolderId = Guid.Parse("bb900986-6117-40f5-8d2e-f72a32ea5185")
-                };
-                TripsEngineClient tripsEngineClient = new TripsEngineClient();
-                CompleteBookingRS result = await tripsEngineClient.CompleteBookingAsync(CompleteBookingRequestParser);
-                return result;
+                        TripFolderId = Guid.Parse("bb900986-6117-40f5-8d2e-f72a32ea5185")
+                    };
+                }
+                catch(Exception ex)
+                {
+                    Logger.Log.LogError(ex);
+                }
 
+                try
+                {
+                    TripsEngineClient tripsEngineClient = new TripsEngineClient();
+                    CompleteBookingRS result = await tripsEngineClient.CompleteBookingAsync(CompleteBookingRequestParser);
+                    return result;
+                }
+                catch(Exception ex)
+                {
+                    Logger.Log.LogError(ex);
+                }
             }
         }
 
