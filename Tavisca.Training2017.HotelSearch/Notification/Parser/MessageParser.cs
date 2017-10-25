09eb3f76acc7a02ea3e9970873e33abef704c981
Bandana Mahapatra <apoorv.pandey9524@gmail.com>
2017-10-25 16:50:42 +0530

    Added function to send mail

---
+++ Tavisca.Training2017.HotelSearch/Notification/Parser/MessageParser.cs
@@ -0,0 +1,77 @@
+﻿using Notification.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notification.Parser
+{
+    public class MessageParser
+    {
+        private Dictionary<Attachment[], AttachmentData[]> dict = new Dictionary<Attachment[], AttachmentData[]>();
+        MailMessage mailMessage;
+        public MessageParser()
+        {
+            mailMessage = new MailMessage();
+        }
+
+        public async Task<MailMessage> ParserAsync(Email email)
+        {
+            mailMessage.BCC = email.BCCField;
+            mailMessage.CC = email.CCField;
+            mailMessage.Id = email.GuidId;
+            mailMessage.SentDate = email.SentDateField;
+            mailMessage.Status = Parse(email.DelieveryStatus);
+            mailMessage.IsHtml = email.IsHtmlField;
+            mailMessage.From = email.FromField;
+            mailMessage.To = email.ToField;
+            mailMessage.Subject = email.SubjectField;
+            mailMessage.Tags = email.TagsField;
+            mailMessage.Attachments = Parser(email.AttachmentsField);
+            mailMessage.Body = email.Body;
+            return  mailMessage;
+        }
+        private Dictionary<EmailDelieveryStatus, DeliveryStatus> dictionary = new Dictionary<EmailDelieveryStatus, DeliveryStatus>()
+        {
+            {
+                EmailDelieveryStatus.All,DeliveryStatus.All
+             },
+            {
+                EmailDelieveryStatus.Sent,DeliveryStatus.Sent
+             },
+            {
+                EmailDelieveryStatus.SentSuccessful,DeliveryStatus.SentSuccessful
+             },
+            {
+                EmailDelieveryStatus.FailureRetry,DeliveryStatus.FailureRetry
+             },
+            {
+                EmailDelieveryStatus.Failed, DeliveryStatus.Failed
+            }
+        };
+
+        public DeliveryStatus Parse(EmailDelieveryStatus mailMessage)
+        {
+            return dictionary[mailMessage];
+        }
+        public Attachment[] Parser(AttachmentData[] attachmentData)
+        {
+            Attachment[] attach = null;
+            for (int i = 0; i < 3; i++)
+            {
+                if ((mailMessage.AttachmentsField[i].FileName == attachmentData[i].FileNameField) && (mailMessage.AttachmentsField[i].FileExtension == attachmentData[i].FileExtensionField) && (mailMessage.AttachmentsField[i].AttachmentFile == attachmentData[i].AttachmentFileField)
+                {
+                    attach[i] = (Attachment)attachmentData[i];
+                    dict.Add(attach, attachmentData);
+                }
+            }
+           
+            return attach;
+        }
+
+
+    }
+}
+
+
+
---
+++ Tavisca.Training2017.HotelSearch/Tavisca.Training2017.HotelSearch/Controllers/Notification.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tavisca.Training2017.HotelSearch.Controllers
+{
+    [Route("/Notification")]
+    public class Notification
+    {
+
+        [Route("Get/Notification")]
+        public async Task GetNotification([FromBody] string requestData)
+        {
+
+        }
+    }
+}
