393c89c0d72dbdd309482220d7840a22918d422c
Bandana Mahapatra <apoorv.pandey9524@gmail.com>
2017-10-25 16:50:42 +0530

    Added function to send mail

---
+++ Tavisca.Training2017.HotelSearch/Notification/Model/EmailDelieveryStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notification.Model
+{
+    public enum EmailDelieveryStatus:int
+    {
+      
+        All = 0,
+        Sent = 1,
+        SentSuccessful = 2,
+        FailureRetry = 3,
+        Failed = 4,
+    }
+}
---
+++ Tavisca.Training2017.HotelSearch/Notification/NotificationDataClient.cs
@@ -0,0 +1,22 @@
+﻿using Notification.Model;
+using Notification.Parser;
+using System;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace Notification
+{
+    public class NotificationDataClient
+    {
+        public async Task<MailMessage> NotifyByMail(Email email)
+        {
+            MailClient mailClient = new MailClient();
+            MessageParser parser = new MessageParser();
+            Message message = new Message();
+            message.
+            message.
+            string str = (await mailClient.SendMailAsync(null, message)).ToString();
+            MailMessage mailMessage = new MailMessage();
+        }
+    }
+}
