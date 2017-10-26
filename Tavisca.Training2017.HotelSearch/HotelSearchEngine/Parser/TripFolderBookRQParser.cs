1c0587777426411a873c6b8e5de83307fbdeadde
Prabhash Mohanty <pmohanty@tavisca.com>
2017-10-26 10:28:24 +0530

    Fixed supllier issue

--- Tavisca.Training2017.HotelSearch/HotelSearchEngine/Parser/TripFolderBookRQParser.cs
+++ Tavisca.Training2017.HotelSearch/HotelSearchEngine/Parser/TripFolderBookRQParser.cs
@@ -174,7 +174,7 @@ namespace HotelSearchEngine.Parser
                             new StateBag(){ Name="Boyd Gaming"},
                             new StateBag(){ Name="IsPassportRequired" , Value="false"}
                         },
-                        Email="bmahapatra@tavisca.com",
+                        Email=request.Email_Id,
                         FirstName="Sandbox",
                         Gender=Gender.Male,
                         KnownTravelerNumber="789456",
