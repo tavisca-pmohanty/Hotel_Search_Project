d5ed1f9fa182c90f4517451a37036791f0d641ce
prabhash moahnty <say2prabhash@outlook.com>
2017-10-30 16:10:12 +0530

    Added function to store order status

--- Tavisca.Training2017.HotelSearch/HotelSearchEngine/Parser/CompleteBookingResponseParser.cs
+++ Tavisca.Training2017.HotelSearch/HotelSearchEngine/Parser/CompleteBookingResponseParser.cs
@@ -1,8 +1,10 @@
 ﻿using APITripEngine;
 using HotelSearchEngine.Contracts;
 using HotelSearchEngine.Model;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +14,7 @@ namespace HotelSearchEngine.Parser
 {
     public class CompleteBookingResponseParser
     {
+        private string _filePath = "D:\\Hotel_Search_Project\\Tavisca.Training2017.HotelSearch\\HotelSearchEngine\\BookingData\\BookingInfo.txt";
         CompleteBookingResponse completeBookingResponse;
         public CompleteBookingResponseParser()
         {
@@ -19,7 +22,7 @@ namespace HotelSearchEngine.Parser
         }
         public async Task<IResponse> ResponseParserAsync(CompleteBookingRS completeBookingRS)
         {
-            completeBookingResponse.TransactionId = completeBookingRS.TripFolder.ConfirmationNumber;
+            completeBookingResponse.TransactionId = completeBookingRS.SessionId;
             HotelTripProduct product = (HotelTripProduct)completeBookingRS.TripFolder.Products[0];
             completeBookingResponse.HotelName = product.HotelItinerary.HotelProperty.Name;
             completeBookingResponse.RoomName = product.HotelItinerary.Rooms[0].RoomName;
@@ -27,7 +30,27 @@ namespace HotelSearchEngine.Parser
             completeBookingResponse.CheckOutDate = product.HotelItinerary.StayPeriod.End;
             completeBookingResponse.NumOfNights = product.HotelItinerary.StayPeriod.Duration;
             completeBookingResponse.Status = completeBookingRS.ServiceStatus.Status.ToString();
+            StoreBookingStatus(completeBookingRS.SessionId,completeBookingRS.ServiceStatus.Status.ToString());
             return completeBookingResponse;
+        }
+
+        private void StoreBookingStatus(string sessionId, string status)
+        {
+            BookingRecord bookingRecord = new BookingRecord();
+            bookingRecord.Status = status;
+            bookingRecord.BookingId = sessionId;
+            TextWriter writer = null;
+            try
+            {
+                var contentsToWriteToFile = JsonConvert.SerializeObject(bookingRecord);
+                writer = new StreamWriter(_filePath,true);
+                writer.Write(contentsToWriteToFile);
+            }
+            finally
+            {
+                if (writer != null)
+                    writer.Close();
+            }
         }
     }
 }
